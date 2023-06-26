16576d3aa00bffb2711ddaaa2cd9eed8d8ae0fa5
Tekkieware <tekkieware@gmail.com>
2023-06-26 16:51:46 +0100

    data fetch and display

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { Country } from './models';
+import axios from 'axios'
 
-function App() {
+
+
+
+
+const App:React.FC = () => {
+const [countriesList, setCountriesList] = useState<Country[]>([])
+
+
+useEffect (()=>{
+  const config = {
+    method: 'get',
+    url: 'https://restcountries.com/v2/all?fields=name,region,area',
+  };
+
+axios(config)
+.then(function (response) {
+  setCountriesList(response.data)
+  console.log(countriesList)
+})
+.catch(function (error) {
+  console.log(error);
+});
+},[countriesList])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="countries-list">
+      <h3>Countries List</h3>
+      {countriesList.map((country) => (
+        <div className='country'>
+        <p className='country-name'>{country.name}</p>
+        <p className='country-region'>{country.region}</p>
+        <p className='country-area'>{country.area}</p>
+        </div>
+      )
+      )}
     </div>
   );
 }
---
+++ src/models.ts
@@ -0,0 +1,7 @@
+
+export interface Country{
+    name: string,
+    region: string,
+    area: number,
+    independent: boolean,
+}
