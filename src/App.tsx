eca8391df7188a527575579cec4b8341e633156a
Tekkieware <tekkieware@gmail.com>
2023-06-28 12:17:04 +0100

    pagination done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import { Country } from './models';
 import axios from 'axios'
-
-
+import Pagination from 'react-bootstrap/Pagination';
 
 
 
 const App:React.FC = () => {
+  
+const [currentPage, setCurrentPage] = useState<number>(1)
+const countriesPerPage: number = 10
+const lastIndex: number = currentPage * countriesPerPage
+const firstIndex: number = lastIndex - countriesPerPage
 const [countriesList, setCountriesList] = useState<Country[]>([])
+const currentPageData: Country[] = countriesList.slice(firstIndex, lastIndex)
+const noOfPages: number = Math.ceil(countriesList.length / countriesPerPage)
+const pageNumbers: number[] = [...Array(noOfPages + 1).keys()].slice(1)
+
+const prevPage = () =>{
+if(currentPage !== 1){
+  setCurrentPage(currentPage - 1)
+}
+}
+
+const nextPage = () =>{
+  if(currentPage !== noOfPages){
+    setCurrentPage(currentPage + 1)
+  }
+}
+
+const changeCurrentPage = (number:number) =>{
+setCurrentPage(number)
+}
 
 
 useEffect (()=>{
@@ -28,15 +51,24 @@ axios(config)
 
   return (
     <div className="countries-list">
-      <h3>Countries List</h3>
-      {countriesList.map((country) => (
-        <div className='country'>
+      <h4>Countries List</h4>
+      {currentPageData.map((country, index) => (
+        <div className='country' key={index}>
         <p className='country-name'>{country.name}</p>
         <p className='country-region'>{country.region}</p>
         <p className='country-area'>{country.area}</p>
         </div>
       )
       )}
+      <Pagination>
+       <Pagination.Prev onClick={prevPage} />
+        {
+          pageNumbers.map((number, index) =>(
+            <Pagination.Item className={number === currentPage ? 'active': ''} key={index} onClick={()=>changeCurrentPage(number)}>{number}</Pagination.Item>
+          ))
+        }
+       <Pagination.Next onClick={nextPage} />
+      </Pagination>
     </div>
   );
 }
